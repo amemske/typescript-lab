fecb5264bf13830084a470c2b54a1539ca1f6254
amemske <olywave@gmail.com>
2023-11-10 18:00:01 +0300

    adding ts to react

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Header from './Header'
+
+function App() {
+
+  return (
+    <>
+      <Header title='Hello Africa' color='brown'/>
+    </>
+  )
+}
+
+export default App
